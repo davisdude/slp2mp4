2090d74dd41029bd96da948963ed6df58eb9c811
Davis C <davisclaib@gmail.com>
2025-06-15 23:43:07 -0500

    Try pillow
    And matplotlib for fonts, since that's the best way to do it for some reason

--- src/slp2mp4/scoreboard/default.py
+++ src/slp2mp4/scoreboard/default.py
@@ -1,94 +1,127 @@
 from slp2mp4.scoreboard import scoreboard
 
-HTML_STR = r"""
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-        <div class="tournament-name">{TOURNAMENT_NAME}</div>
-        <div class="filler"><hr></div>
+from PIL import Image, ImageDraw, ImageFont
+from matplotlib import font_manager
 
-        <div id="bottom">
-            <div class="filler"><hr></div>
 
-            <div class="combatant">
-                <span class="combatant-name">{COMBATANT_1_NAME}</span>:
-                <span class="combatant-score">{COMBATANT_1_SCORE}</span>
-            </div>
+_MONO_FONT_PROPS = font_manager.FontProperties(
+    family=["Inconsolata", "Consolas", "monospace"],
+)
 
-            <div class="combatant">
-                <span class="combatant-name">{COMBATANT_2_NAME}</span>:
-                <span class="combatant-score">{COMBATANT_2_SCORE}</span>
-            </div>
+# TODO: Clean this up. Like a lot.
+class GameInfoPanel(scoreboard.ScoreboardPanel):
+    def draw(self, png_path, height, context_data, game_index):
+        y = 0
+        foreground_color = (255, 255, 255)
+        width = self.get_width(height)
+        padding = height // 200
+        size = (width, height)
+        self.name = png_path
+        # For some reason, using RGBA makes the text look bold
+        self.image = Image.new("RGB", size)
+        draw = ImageDraw.Draw(self.image)
+        font_path = font_manager.fontManager.findfont(_MONO_FONT_PROPS)
+        big_font = ImageFont.truetype(font_path, height // 12)
+        regular_font = ImageFont.truetype(font_path, height // 24)
 
-            <div class="filler"><hr></div>
+        # Top
+        y += padding
+        (_, _, _, y) = scoreboard.draw_multiline_text(
+            y,
+            padding,
+            width,
+            draw,
+            context_data["startgg"]["tournament"]["name"],
+            font=big_font,
+            fill=foreground_color,
+        )
+        y += padding
 
-            <div class="bracket-info">
-                <span class="bracket-info-round">{BRACKET_ROUND}</span>
-                <span class="bracket-info-scoring">{BRACKET_SCORING}</span>
-            </div>
-        </div>
-    </body>
-</html>
-"""
+        draw.line(
+            ((padding, y), (width - padding, y)),
+            fill=foreground_color,
+            width=height // 480,
+        )
+        y += padding
 
-CSS_STR = """
-body {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    color: white;
-    background-color: black;
-    font-family: "Inconsolata", "Consolas", "monospace";
-}
+        # Bottom
 
-div {
-    margin-left: 0.5em;
-    margin-right: 0.5em;
-}
+        y = height - padding
+        (_, y, _, _) = scoreboard.draw_multiline_text(
+            y,
+            padding,
+            width,
+            draw,
+            f"Best of {context_data['bestOf']}",
+            align="left",
+            anchor="bottom",
+            font=regular_font,
+            fill=foreground_color,
+        )
+        y -= padding
 
-#bottom {
-    position: absolute;
-    bottom: 0px;
-    width: inherit;
-    margin-left: inherit;
-    margin-right: inherit;
-}
+        (_, y, _, _) = scoreboard.draw_multiline_text(
+            y,
+            padding,
+            width,
+            draw,
+            context_data["startgg"]["set"]["fullRoundText"],
+            align="left",
+            anchor="bottom",
+            font=regular_font,
+            fill=foreground_color,
+        )
+        y -= padding
 
-.tournament-name {
-    text-align: center;
-    font-size: 5em;
-}
+        draw.line(
+            ((padding, y), (width - padding, y)),
+            fill=foreground_color,
+            width=height // 480,
+        )
+        y -= padding
 
-.combatant {
-    font-size: 2.5em;
-}
+        (_, y, _, _) = scoreboard.draw_multiline_text(
+            y,
+            padding,
+            width,
+            draw,
+            scoreboard.get_name_and_score_from_slot_data(
+                context_data["scores"][game_index]["slots"][1]
+            ),
+            align="left",
+            anchor="bottom",
+            font=regular_font,
+            fill=foreground_color,
+        )
+        y -= padding
 
-.combatant-name {
-    text-align: left;
-}
+        (_, y, _, _) = scoreboard.draw_multiline_text(
+            y,
+            padding,
+            width,
+            draw,
+            scoreboard.get_name_and_score_from_slot_data(
+                context_data["scores"][game_index]["slots"][0]
+            ),
+            align="left",
+            anchor="bottom",
+            font=regular_font,
+            fill=foreground_color,
+        )
+        y -= padding
 
-.combatant-score {
-    float: right;
-}
-
-.bracket-info {
-    font-size: 2.5em;
-}
-
-.bracket-info-round {
-    text-align: left;
-}
-
-.bracket-info-scoring {
-    float: right;
-}
-"""
+        draw.line(
+            ((padding, y), (width - padding, y)),
+            fill=foreground_color,
+            width=height // 480,
+        )
+        y -= padding
 
 
 class DefaultScoreboard(scoreboard.Scoreboard):
     def _get_scoreboard_panels(self):
         return [
-            scoreboard.ScoreboardPanel(HTML_STR, CSS_STR, 606 / 1080),
+            GameInfoPanel(606 / 1080)
         ]
 
     def _get_scoreboard_args(self):
--- src/slp2mp4/scoreboard/scoreboard.py
+++ src/slp2mp4/scoreboard/scoreboard.py
@@ -8,28 +8,23 @@ import typing
 
 from slp2mp4 import ffmpeg
 
-from html2image import Html2Image
+from PIL import Image
 
 
 @dataclasses.dataclass
 class ScoreboardPanel:
-    html_str: str
-    css_str: str
     aspect_ratio: float
-    png_path: typing.BinaryIO = dataclasses.field(init=False)
+    name: str = dataclasses.field(init=False, default=None)
+    image: Image = dataclasses.field(init=False, default=None)
 
-    def render(self, height):
-        width = self.aspect_ratio * height
-        hti = Html2Image(
-            size=(int(width), int(height)),
-            output_path=self.png_path.parent,
-        )
-        hti.screenshot(
-            html_str=self.html_str,
-            css_str=self.css_str,
-            save_as=self.png_path.name,
-        )
-        return self.png_path.name
+    def get_width(self, height):
+        return int(self.aspect_ratio * height)
+
+    def draw(self, height, name, context_data, game_index):
+        raise NotImplementedError("draw must be overridden by child")
+
+    def save(self):
+        self.image.save(self.name)
 
 
 # TODO: Pass conf to constructor to allow customization
@@ -43,56 +38,25 @@ class Scoreboard:
         with open(self.context_json_path) as json_data:
             return json.load(json_data)
 
-    def _get_scoreboard_panels(self) -> list[ScoreboardPanel]:
-        raise NotImplementedError("_get_scoreboard_panels must be overridden by child")
-
-    # In the ffmpeg command (ffmpeg.py:merge_audio_and_video()), input 0 =
-    # the audio, input 1 = the video. Remaining inputs are scoreboard images.
-    def _get_scoreboard_args(self):
-        raise NotImplementedError("_get_scoreboard_args must be overridden by child")
-
     def _get_scale_args(self):
         return (f"[1]scale=width=-2:height={self.height}[scaled]",)
 
-    def _update_html(self, panels, context_data):
-        for panel in panels:
-            # TODO: Support challonge
-            html = panel.html_str
-            names = [
-                _get_name_from_slot_data(slot_data)
-                for slot_data in context_data["scores"][self.game_index]["slots"]
-            ]
-            scores = [
-                str(slot_data["score"])
-                for slot_data in context_data["scores"][self.game_index]["slots"]
-            ]
-            html = html.replace(
-                "{TOURNAMENT_NAME}", context_data["startgg"]["tournament"]["name"]
-            )
-            html = html.replace(
-                "{BRACKET_ROUND}", context_data["startgg"]["set"]["fullRoundText"]
-            )
-            html = html.replace("{BRACKET_SCORING}", f"Bo{context_data['bestOf']}")
-            html = html.replace("{COMBATANT_1_NAME}", names[0])
-            html = html.replace("{COMBATANT_2_NAME}", names[1])
-            html = html.replace("{COMBATANT_1_SCORE}", scores[0])
-            html = html.replace("{COMBATANT_2_SCORE}", scores[1])
-            panel.html_str = html
+    def _get_scoreboard_panels(self):
+        raise NotImplementedError("_get_scoreboard_panels must be overridden by child")
 
-    def _render_html(self, panels):
-        for panel in panels:
-            panel.render(self.height)
+    def _get_scoreboard_args(self):
+        raise NotImplementedError("_get_scoreboard_args must be overridden by child")
 
     @contextlib.contextmanager
     def get_args(self):
-        panels = self._get_scoreboard_panels()
         context_data = self._get_context_data()
-        self._update_html(panels, context_data)
         try:
+            panels = self._get_scoreboard_panels()
             with _scoreboard_panel_context_manager(panels) as png_paths:
-                self._render_html(panels)
-                for index, panel in enumerate(panels):
-                    shutil.copyfile(panel.png_path, f"scoreboard_{self.game_index}.png")
+                for panel, png_path in zip(panels, png_paths):
+                    panel.draw(png_path, self.height, context_data, self.game_index)
+                    panel.save()
+                    shutil.copyfile(png_path, f"scoreboard_{self.game_index}.png")
                 scale_args = self._get_scale_args()
                 scoreboard_args = self._get_scoreboard_args()
                 # Don't re-scale if not doing filtering
@@ -108,31 +72,6 @@ class Scoreboard:
             pass
 
 
-def _get_name(name, prefixes, pronouns, ports, is_singles=True):
-    if is_singles:
-        if prefixes:
-            name = f"{prefixes} | {name}"
-        if pronouns:
-            name = f"{name} ({pronouns})"
-    else:
-        name = f"{name} (P{ports})"
-    return name
-
-
-def _get_name_from_slot_data(slot_data):
-    is_singles = len(slot_data["displayNames"]) == 1
-    names = [
-        _get_name(name, prefixes, pronouns, ports, is_singles)
-        for name, prefixes, pronouns, ports in zip(
-            slot_data["displayNames"],
-            slot_data["prefixes"],
-            slot_data["pronouns"],
-            slot_data["ports"],
-        )
-    ]
-    return ("/").join(names)
-
-
 @contextlib.contextmanager
 def _scoreboard_panel_context_manager(panels: list[ScoreboardPanel]):
     png_temps = [
@@ -144,12 +83,71 @@ def _scoreboard_panel_context_manager(panels: list[ScoreboardPanel]):
         png_paths.append(pathlib.Path(png_temp.name))
         png_temp.close()
     try:
-        for panel, png_path in zip(panels, png_paths):
-            panel.png_path = png_path
         yield png_paths
     finally:
         for tmpfile in png_paths:
             tmpfile.unlink()
+
+
+def _get_name(name, prefixes, pronouns, ports, is_singles=True):
+    if is_singles:
+        if prefixes:
+            name = f"{prefixes} | {name}"
+        if pronouns:
+            name = f"{name} ({pronouns})"
+    else:
+        name = f"{name} (P{ports})"
+    return name
+
+
+def get_name_and_score_from_slot_data(slot_data):
+    is_singles = len(slot_data["displayNames"]) == 1
+    names = [
+        _get_name(name, prefixes, pronouns, ports, is_singles)
+        for name, prefixes, pronouns, ports in zip(
+            slot_data["displayNames"],
+            slot_data["prefixes"],
+            slot_data["pronouns"],
+            slot_data["ports"],
+        )
+    ]
+    return f"{('/').join(names)}: {slot_data['score']}"
+
+
+def get_wrapped_text(width, draw, text, font):
+    words = text.split()
+    lines = []
+    current_line = words[0]
+    xy = (0, 0)
+    for word in words[1:]:
+        new_text = f"{current_line} {word}"
+        _, _, text_width, _ = draw.textbbox(xy, new_text, font=font)
+        if text_width > width:
+            lines.append(current_line)
+            current_line = word
+        else:
+            current_line = new_text
+    lines.append(current_line)
+    text = ("\n").join(lines)
+    bbox = draw.multiline_textbbox(xy, text, font=font)
+    return text, bbox
+
+
+def draw_multiline_text(y, padding, width, draw, text, align="center", anchor="top", **kwargs):
+    max_width = width - 2 * padding
+    text, bbox = get_wrapped_text(max_width, draw, text, kwargs["font"])
+    _, _, text_width, text_height = bbox
+    if align == "center":
+        x = (width - text_width) // 2
+    elif align == "left":
+        x = padding
+    elif align == "right":
+        x = width - text_width - padding
+    if anchor == "bottom":
+        y = y - text_height
+    pos = (x, y)
+    draw.multiline_text(pos, text, align="center", **kwargs)
+    return draw.multiline_textbbox(pos, text, font=kwargs["font"])
 
 
 # TODO: Widescreen scoreboard
