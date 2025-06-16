523826174e9e0a149cf0cba166ed887f8a60a4f6
Davis C <davisclaib@gmail.com>
2025-06-16 18:06:12 -0500

    Revert "Try pillow"
    This reverts commit 2090d74dd41029bd96da948963ed6df58eb9c811.

--- src/slp2mp4/scoreboard/scoreboard.py
+++ src/slp2mp4/scoreboard/scoreboard.py
@@ -8,23 +8,28 @@ import typing
 
 from slp2mp4 import ffmpeg
 
-from PIL import Image
+from html2image import Html2Image
 
 
 @dataclasses.dataclass
 class ScoreboardPanel:
+    html_str: str
+    css_str: str
     aspect_ratio: float
-    name: str = dataclasses.field(init=False, default=None)
-    image: Image = dataclasses.field(init=False, default=None)
+    png_path: typing.BinaryIO = dataclasses.field(init=False)
 
-    def get_width(self, height):
-        return int(self.aspect_ratio * height)
-
-    def draw(self, height, name, context_data, game_index):
-        raise NotImplementedError("draw must be overridden by child")
-
-    def save(self):
-        self.image.save(self.name)
+    def render(self, height):
+        width = self.aspect_ratio * height
+        hti = Html2Image(
+            size=(int(width), int(height)),
+            output_path=self.png_path.parent,
+        )
+        hti.screenshot(
+            html_str=self.html_str,
+            css_str=self.css_str,
+            save_as=self.png_path.name,
+        )
+        return self.png_path.name
 
 
 # TODO: Pass conf to constructor to allow customization
@@ -38,25 +43,56 @@ class Scoreboard:
         with open(self.context_json_path) as json_data:
             return json.load(json_data)
 
-    def _get_scale_args(self):
-        return (f"[1]scale=width=-2:height={self.height}[scaled]",)
-
-    def _get_scoreboard_panels(self):
+    def _get_scoreboard_panels(self) -> list[ScoreboardPanel]:
         raise NotImplementedError("_get_scoreboard_panels must be overridden by child")
 
+    # In the ffmpeg command (ffmpeg.py:merge_audio_and_video()), input 0 =
+    # the audio, input 1 = the video. Remaining inputs are scoreboard images.
     def _get_scoreboard_args(self):
         raise NotImplementedError("_get_scoreboard_args must be overridden by child")
 
+    def _get_scale_args(self):
+        return (f"[1]scale=width=-2:height={self.height}[scaled]",)
+
+    def _update_html(self, panels, context_data):
+        for panel in panels:
+            # TODO: Support challonge
+            html = panel.html_str
+            names = [
+                _get_name_from_slot_data(slot_data)
+                for slot_data in context_data["scores"][self.game_index]["slots"]
+            ]
+            scores = [
+                str(slot_data["score"])
+                for slot_data in context_data["scores"][self.game_index]["slots"]
+            ]
+            html = html.replace(
+                "{TOURNAMENT_NAME}", context_data["startgg"]["tournament"]["name"]
+            )
+            html = html.replace(
+                "{BRACKET_ROUND}", context_data["startgg"]["set"]["fullRoundText"]
+            )
+            html = html.replace("{BRACKET_SCORING}", f"Bo{context_data['bestOf']}")
+            html = html.replace("{COMBATANT_1_NAME}", names[0])
+            html = html.replace("{COMBATANT_2_NAME}", names[1])
+            html = html.replace("{COMBATANT_1_SCORE}", scores[0])
+            html = html.replace("{COMBATANT_2_SCORE}", scores[1])
+            panel.html_str = html
+
+    def _render_html(self, panels):
+        for panel in panels:
+            panel.render(self.height)
+
     @contextlib.contextmanager
     def get_args(self):
+        panels = self._get_scoreboard_panels()
         context_data = self._get_context_data()
+        self._update_html(panels, context_data)
         try:
-            panels = self._get_scoreboard_panels()
             with _scoreboard_panel_context_manager(panels) as png_paths:
-                for panel, png_path in zip(panels, png_paths):
-                    panel.draw(png_path, self.height, context_data, self.game_index)
-                    panel.save()
-                    shutil.copyfile(png_path, f"scoreboard_{self.game_index}.png")
+                self._render_html(panels)
+                for index, panel in enumerate(panels):
+                    shutil.copyfile(panel.png_path, f"scoreboard_{self.game_index}.png")
                 scale_args = self._get_scale_args()
                 scoreboard_args = self._get_scoreboard_args()
                 # Don't re-scale if not doing filtering
@@ -72,23 +108,6 @@ class Scoreboard:
             pass
 
 
-@contextlib.contextmanager
-def _scoreboard_panel_context_manager(panels: list[ScoreboardPanel]):
-    png_temps = [
-        tempfile.NamedTemporaryFile(suffix=".png", delete=False)
-        for _ in panels
-    ]
-    png_paths = []
-    for png_temp in png_temps:
-        png_paths.append(pathlib.Path(png_temp.name))
-        png_temp.close()
-    try:
-        yield png_paths
-    finally:
-        for tmpfile in png_paths:
-            tmpfile.unlink()
-
-
 def _get_name(name, prefixes, pronouns, ports, is_singles=True):
     if is_singles:
         if prefixes:
@@ -100,7 +119,7 @@ def _get_name(name, prefixes, pronouns, ports, is_singles=True):
     return name
 
 
-def get_name_and_score_from_slot_data(slot_data):
+def _get_name_from_slot_data(slot_data):
     is_singles = len(slot_data["displayNames"]) == 1
     names = [
         _get_name(name, prefixes, pronouns, ports, is_singles)
@@ -111,43 +130,26 @@ def get_name_and_score_from_slot_data(slot_data):
             slot_data["ports"],
         )
     ]
-    return f"{('/').join(names)}: {slot_data['score']}"
+    return ("/").join(names)
 
 
-def get_wrapped_text(width, draw, text, font):
-    words = text.split()
-    lines = []
-    current_line = words[0]
-    xy = (0, 0)
-    for word in words[1:]:
-        new_text = f"{current_line} {word}"
-        _, _, text_width, _ = draw.textbbox(xy, new_text, font=font)
-        if text_width > width:
-            lines.append(current_line)
-            current_line = word
-        else:
-            current_line = new_text
-    lines.append(current_line)
-    text = ("\n").join(lines)
-    bbox = draw.multiline_textbbox(xy, text, font=font)
-    return text, bbox
-
-
-def draw_multiline_text(y, padding, width, draw, text, align="center", anchor="top", **kwargs):
-    max_width = width - 2 * padding
-    text, bbox = get_wrapped_text(max_width, draw, text, kwargs["font"])
-    _, _, text_width, text_height = bbox
-    if align == "center":
-        x = (width - text_width) // 2
-    elif align == "left":
-        x = padding
-    elif align == "right":
-        x = width - text_width - padding
-    if anchor == "bottom":
-        y = y - text_height
-    pos = (x, y)
-    draw.multiline_text(pos, text, align="center", **kwargs)
-    return draw.multiline_textbbox(pos, text, font=kwargs["font"])
+@contextlib.contextmanager
+def _scoreboard_panel_context_manager(panels: list[ScoreboardPanel]):
+    png_temps = [
+        tempfile.NamedTemporaryFile(suffix=".png", delete=False)
+        for _ in panels
+    ]
+    png_paths = []
+    for png_temp in png_temps:
+        png_paths.append(pathlib.Path(png_temp.name))
+        png_temp.close()
+    try:
+        for panel, png_path in zip(panels, png_paths):
+            panel.png_path = png_path
+        yield png_paths
+    finally:
+        for tmpfile in png_paths:
+            tmpfile.unlink()
 
 
 # TODO: Widescreen scoreboard
