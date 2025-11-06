3619d6b3b0de7704134a3af4ef53a072e242cf16
Davis <4967021+davisdude@users.noreply.github.com>
2025-11-06 07:58:36 -0600

    Fix no scoreboard

--- src/slp2mp4/scoreboard/none.py
+++ src/slp2mp4/scoreboard/none.py
@@ -2,7 +2,7 @@ from slp2mp4.scoreboard import scoreboard
 
 
 class NoScoreboard(scoreboard.Scoreboard):
-    def _get_scoreboard_panels(self):
+    def _get_scoreboard_panels(self, _num_teams: int):
         return []
 
     def _get_scoreboard_args(self):
