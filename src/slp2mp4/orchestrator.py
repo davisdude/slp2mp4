52749f9d88fe61dd2b7393aecaed9688e37cdd2b
Davis C <davisclaib@gmail.com>
2026-02-22 18:12:41 -0600

    Parallel test

--- src/slp2mp4/orchestrator.py
+++ src/slp2mp4/orchestrator.py
@@ -1,11 +1,3 @@
-# Commonizes the batching / concatenating of slippi files
-#
-# This renders by "set," which will be slightly slower on average than rendering
-# all videos then concat-ing when the set is finished, but has a few upsides:
-#
-#   1. Reduces memory usage
-#   2. Simplifies implementation
-
 import concurrent.futures
 import dataclasses
 import enum
@@ -20,24 +12,9 @@ import slp2mp4.video as video
 from slp2mp4.output import Output
 
 
-def render_and_concat(
-    kill_event: multiprocessing.Event,
-    executor: concurrent.futures.Executor,
-    conf: dict,
-    output: Output,
-):
-    ffmpeg_runners = [FfmpegRunner(conf) for _ in output.inputs]
-    dolphin_runners = [DolphinRunner(conf, kill_event) for _ in output.inputs]
-    futures = {
-        i: executor.submit(render, fr, dr, i)
-        for fr, dr, i in zip(ffmpeg_runners, dolphin_runners, output.inputs)
-    }
-    concurrent.futures.wait(futures.values())
-    tmp_paths = [futures[i].result() for i in output.inputs]
-    concat(conf, output.output, tmp_paths)
-
-
-def render(ffmpeg_runner, dolphin_runner, slp_path: pathlib.Path):
+def render(conf: dict, slp_path: pathlib.Path, kill_event: multiprocessing.Event):
+    ffmpeg_runner = FfmpegRunner(conf)
+    dolphin_runner = DolphinRunner(conf, kill_event)
     tmp = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False)
     tmp_path = pathlib.Path(tmp.name)
     video.render(ffmpeg_runner, dolphin_runner, slp_path, tmp_path)
@@ -45,7 +22,12 @@ def render(ffmpeg_runner, dolphin_runner, slp_path: pathlib.Path):
     return tmp_path
 
 
-def concat(conf: dict, output_path: pathlib.Path, renders: list[pathlib.Path]):
+def concat(
+    conf: dict,
+    output_path: pathlib.Path,
+    renders: list[concurrent.futures.Future[pathlib.Path]],
+):
+    renders = list(renders)
     Ffmpeg = FfmpegRunner(conf)
     output_path.parent.mkdir(parents=True, exist_ok=True)
     Ffmpeg.concat_videos(renders, output_path)
@@ -53,8 +35,16 @@ def concat(conf: dict, output_path: pathlib.Path, renders: list[pathlib.Path]):
         render.unlink()
 
 
-def run(event: multiprocessing.Event, conf: dict, outputs: list[Output]):
-    num_procs = conf["runtime"]["parallel"]
-    with concurrent.futures.ProcessPoolExecutor(num_procs) as executor:
-        for output in outputs:
-            render_and_concat(event, executor, conf, output)
+def run(kill_event: multiprocessing.Event, conf: dict, outputs: list[Output]):
+    num_workers = conf["runtime"]["parallel"]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as render_pool:
+        with concurrent.futures.ThreadPoolExecutor() as concat_pool:
+            for output in outputs:
+                confs = (conf for _ in output.inputs)
+                kill_events = (kill_event for _ in output.inputs)
+                render_futures = render_pool.map(
+                    render, confs, output.inputs, kill_events
+                )
+                concat_futures = concat_pool.submit(
+                    concat, conf, output.output, render_futures
+                )
