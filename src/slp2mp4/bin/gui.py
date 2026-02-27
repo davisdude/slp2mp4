0f467e7aaeea05b3e9c5ad1ebf49e197af9e81fb
Davis C <davisclaib@gmail.com>
2026-02-27 07:39:30 -0600

    Update GUI for dry run
    Fixes #34

--- src/slp2mp4/bin/gui.py
+++ src/slp2mp4/bin/gui.py
@@ -681,11 +681,11 @@ class Slp2Mp4GUI:
             paths = [pathlib.Path(self.input_var.get())]
             output_directory = pathlib.Path(self.output_var.get())
             dry_run = self.dry_run_var.get()
-            mode = modes.MODES[self.mode_var.get()].mode(paths, output_directory)
+            mode = modes.MODES[self.mode_var.get()].mode(paths, output_directory, dry_run)
             manager = multiprocessing.Manager()
             event = manager.Event()
             self.queue.put(("log", "Starting conversion..."))
-            with mode.run(event, dry_run) as (executor, future):
+            with mode.run(event) as (executor, future):
                 while True:
                     if self.stop:
                         event.set()
