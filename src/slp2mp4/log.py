96b905718bdf636b3cedab8b69489a10c4ee781b
Davis C <davisclaib@gmail.com>
2026-03-22 13:23:58 -0500

    Change tk_found from dunder

--- src/slp2mp4/log.py
+++ src/slp2mp4/log.py
@@ -3,21 +3,21 @@ from __future__ import annotations
 import io
 import logging
 
-__TK_FOUND = True
+_TK_FOUND = True
 try:
     import tkinter as tk
 except (ImportError, ModuleNotFoundError):
-    __TK_FOUND = False
+    _TK_FOUND = False
 
 class TkStream(io.StringIO):
     def __init__(self, textobj: "tk.Text", *args, **kwargs):
-        if not __TK_FOUND:
+        if not _TK_FOUND:
             return
         super().__init__(*args, **kwargs)
         self.textobj = textobj
 
     def write(self, s):
-        if not __TK_FOUND:
+        if not _TK_FOUND:
             return
         super().write(s)
         new_text = self.read()
@@ -42,7 +42,7 @@ def update_logger(debug=False, tk_log_text=None):
     stdout_stream.setFormatter(formatter)
     logger.addHandler(stdout_stream)
 
-    if tk_log_text and __TK_FOUND:
+    if tk_log_text and _TK_FOUND:
         tk_stream = logging.StreamHandler(TkStream(tk_log_text))
         tk_stream.setFormatter(formatter)
         logger.addHandler(tk_stream)
